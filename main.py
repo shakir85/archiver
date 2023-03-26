c4c28eaff636444783dcb02c30b1666394e49b64
Shakir <35549045+shakir85@users.noreply.github.com>
2023-03-25 21:26:28 -0700

    Fix trailing slashes in paths (#2)

--- main.py
+++ main.py
@@ -2,17 +2,29 @@
 from app import tar
 from argparse import ArgumentParser
 
-if __name__ == '__main__':
 
+def arguments():
     parser = ArgumentParser(prog='PROG', description='''Lightweight archiving tool''')
-
     parser.add_argument('--archive', required=True, action='store_true', help='tar a directory')
-    parser.add_argument('-s', '--src', required=True, help='Absolute path to source directory. No trailing forward-slash')
-    parser.add_argument('-d', '--dst', required=False, help='Absolute path to destination. No trailing forward-slash')
+    parser.add_argument('-s', '--src', required=True, help='Absolute path to source directory. No trailing slash')
+    parser.add_argument('-d', '--dst', required=False, help='Absolute path to destination. No trailing slash')
     parser.add_argument('-n', '--jobname', help="Task name, use any meaningful name")
     parser.add_argument('--dry-run', required=False, action='store_true')
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def remove_trail_slash(path):
+    if path.endswith('/'):
+        path = path[:-1]
+    return path
+
+
+if __name__ == '__main__':
+
+    args = arguments()
+    src = remove_trail_slash(args.src)
+    dst = remove_trail_slash(args.dst)
 
     if args.archive:
-        tar.compress_to_dest(src=args.src, dst=args.dst, job_name=args.jobname, dry_run=args.dry_run)
+        tar.compress_to_dest(src=src, dst=dst, job_name=args.jobname, dry_run=args.dry_run)
